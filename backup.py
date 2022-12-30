66397983483a5fa638be121333a20058550a9d26
trackz <66169371+jvsc3@users.noreply.github.com>
2022-12-30 00:40:48 -0800

    Create backup.py

---
+++ backup.py
@@ -0,0 +1,19 @@
+import os
+import time
+import datetime
+
+MYSQL_HOST = "localhost"
+MYSQL_USERNAME = "root"
+DATABASE_NAME = "trenches"
+BACKUP_DIRECTORY = "~/Desktop"
+BACKUP_INTERVAL = 43200 # 12 saat
+
+while True:
+    now = datetime.datetime.now()
+    filename = f"{DATABASE_NAME}_{now:%Y-%m-%d_%H-%M-%S}.sql"
+    filepath = os.path.join(BACKUP_DIRECTORY, filename)
+
+    command = f"mysqldump -h {MYSQL_HOST} -u {MYSQL_USERNAME} -p{MYSQL_PASSWORD} {DATABASE_NAME} > {filepath}"
+    os.system(command)
+
+    time.sleep(BACKUP_INTERVAL)
